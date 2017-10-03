777a74d96ce0fbf37b6a72eef5919e1f0d02b213
Aram <avci.aram@gmail.com>
2017-10-03 20:05:04 +0200

    login.html with javascript

---
+++ script/app.js
@@ -0,0 +1,7 @@
+var login = function (username, password) {
+    if (username == "username" && password == "password") {
+        window.location.replace("student.html");
+    } else {
+        window.location.replace("login.html");
+    }
+}
