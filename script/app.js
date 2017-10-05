4ab2ae67ba2c7aac45f0f4d811207896123e1a96
kyhrikard <rikard.carlsson@student.kyh.se>
2017-10-05 11:58:09 +0200

    Add erromessage and ENTER press to login site

--- script/app.js
+++ script/app.js
@@ -1,7 +1,23 @@
-var login = function (username, password) {
+var login = function () {
+    let username = document.forms["login-form"]["username"].value;
+    let password = document.forms["login-form"]["password"].value;
     if (username == "username" && password == "password") {
-        window.location.replace("student.html");
-    } else {
-        window.location.replace("login.html");
+        return true;
     }
+    else if (username == "" || password == "") {
+        document.getElementById('errorMessageDiv').style.color = "black";
+        document.getElementById('errorMessageDiv').innerText = "Username or password cannot be blank";
+        return false;
+    }
+    else {
+        setTimeout(printMessageToDOM, 1000);
+        document.getElementById('errorMessageDiv').style.color = "black";
+        document.getElementById('errorMessageDiv').innerText = "Checking DB....";
+        return false;
+    }
+}
+
+function printMessageToDOM() {
+    document.getElementById('errorMessageDiv').style.color = "red";
+    document.getElementById('errorMessageDiv').innerText = "Invalid username or password";
 }
