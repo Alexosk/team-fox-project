5704ddaad636c7473ecee73997caef002c333e42
kyhrikard <rikard.carlsson@student.kyh.se>
2017-10-05 15:00:35 +0200

    Improve error messages for login site

--- script/app.js
+++ script/app.js
@@ -1,23 +1,27 @@
 var login = function () {
     let username = document.forms["login-form"]["username"].value;
     let password = document.forms["login-form"]["password"].value;
-    if (username == "username" && password == "password") {
-        return true;
+    if ((username == "username" && password == "password") || (username == "student" && password == "123")) {
+        printMessageToDOM("black", "Redirecting.....");
+        setTimeout(redirectToStudent, 1500);
+        return false;
     }
     else if (username == "" || password == "") {
-        document.getElementById('errorMessageDiv').style.color = "black";
-        document.getElementById('errorMessageDiv').innerText = "Username or password cannot be blank";
+        printMessageToDOM("red", "User name or password cannot be blank");
         return false;
     }
     else {
-        setTimeout(printMessageToDOM, 1000);
-        document.getElementById('errorMessageDiv').style.color = "black";
-        document.getElementById('errorMessageDiv').innerText = "Checking DB....";
+        setTimeout(printMessageToDOM, 1500, "red", "Invalid user name or password\n Try username: student password: 123");
+        printMessageToDOM("black", "Checking DB.....");
         return false;
     }
 }
 
-function printMessageToDOM() {
-    document.getElementById('errorMessageDiv').style.color = "red";
-    document.getElementById('errorMessageDiv').innerText = "Invalid username or password";
+function printMessageToDOM(color, message) {
+    document.getElementById('errorMessageDiv').style.color = color;
+    document.getElementById('errorMessageDiv').innerText = message;
+}
+
+function redirectToStudent() {
+    window.location = "student.html";
 }
