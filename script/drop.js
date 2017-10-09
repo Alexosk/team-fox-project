84c0443d4ebfa4136fd7df2292bed65d42e4fa25
kyhrikard <rikard.carlsson@student.kyh.se>
2017-10-09 17:00:57 +0200

    Disable jQuery logon on mobile devices

--- script/drop.js
+++ script/drop.js
@@ -1,17 +1,31 @@
-$(document).ready(function(){
-    var tri = $(".triangle");
-    var form = $(".login-form");
-    var status = false;
-      $("#login").click(function(event){
-        event.preventDefault();
-        if(status == false){
-          tri.fadeIn();
-          form.fadeIn();
-          status = true;
-        }else{
-          tri.fadeOut();
-          form.fadeOut();
-          status = false;
-        }
-      })
+$(document).ready(function () {
+  var tri = $(".triangle");
+  var form = $(".login-form");
+  var status = false;
+  // If window is smaller than 1000px than skip the jQuery log-in. 
+  $("#login").click(function (event) {
+    if (mobileSize()){
+      return;
+    }
+    event.preventDefault();
+    if (status == false) {
+      tri.fadeIn();
+      form.fadeIn();
+      status = true;
+    } else {
+      tri.fadeOut();
+      form.fadeOut();
+      status = false;
+    }
+  })
 })
+
+// Checks if window size is less than 1000px and in that case sets the login to login.html
+function mobileSize() {
+  if ($(window).width() < 1000){
+    document.getElementById("login").href = "login.html";
+    return true;
+  }
+  return false;
+}
+
