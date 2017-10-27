7a97b44317e584410f8318e84ab34c0777ec867a
kyhrikard <rikard.carlsson@student.kyh.se>
2017-10-27 02:37:11 +0200

    More smileys

--- script/smileySurvey.js
+++ script/smileySurvey.js
@@ -4,18 +4,24 @@ function hideSmileySurvey(feeling, liNumber) {
     switch (liNumber) {
         case 1:
             document.getElementById("smileyList").children[0].style.fontSize = "350%";
-            document.getElementById("smileyList").children[1].style.fontSize = "150%";
-            document.getElementById("smileyList").children[2].style.fontSize = "150%";
+            document.getElementById("smileyList").children[1].style.fontSize = "200%";
+            document.getElementById("smileyList").children[2].style.fontSize = "200%";
+            document.getElementById("smileyList").children[1].innerHTML = "&#128553";
+            document.getElementById("smileyList").children[2].innerHTML = "&#128553";
             break;
         case 2:
-            document.getElementById("smileyList").children[0].style.fontSize = "150%";
+            document.getElementById("smileyList").children[0].style.fontSize = "200%";
             document.getElementById("smileyList").children[1].style.fontSize = "350%";
-            document.getElementById("smileyList").children[2].style.fontSize = "150%";
+            document.getElementById("smileyList").children[2].style.fontSize = "200%";
+            document.getElementById("smileyList").children[0].innerHTML = "&#128528";
+            document.getElementById("smileyList").children[2].innerHTML = "&#128528";
             break;
         case 3:
-            document.getElementById("smileyList").children[0].style.fontSize = "150%";
-            document.getElementById("smileyList").children[1].style.fontSize = "150%";
+            document.getElementById("smileyList").children[0].style.fontSize = "200%";
+            document.getElementById("smileyList").children[1].style.fontSize = "200%";
             document.getElementById("smileyList").children[2].style.fontSize = "350%";
+            document.getElementById("smileyList").children[0].innerHTML = "&#128512";
+            document.getElementById("smileyList").children[1].innerHTML = "&#128512";
             break;
     }
 
