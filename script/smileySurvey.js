8302ebab1007c00d924b0137ad8437d30129107c
kyhrikard <rikard.carlsson@student.kyh.se>
2017-10-27 02:03:07 +0200

    Add feedback from smiley survey and order it to always be on top

--- script/smileySurvey.js
+++ script/smileySurvey.js
@@ -1,3 +1,25 @@
-function hideSmileySurvey() {
-    document.getElementById("smileySurvey").style.display = "none";
+function hideSmileySurvey(feeling, liNumber) {
+    document.getElementById("smileyHeading").innerHTML = feeling;
+
+    switch (liNumber) {
+        case 1:
+            document.getElementById("smileyList").children[0].style.fontSize = "350%";
+            document.getElementById("smileyList").children[1].style.fontSize = "150%";
+            document.getElementById("smileyList").children[2].style.fontSize = "150%";
+            break;
+        case 2:
+            document.getElementById("smileyList").children[0].style.fontSize = "150%";
+            document.getElementById("smileyList").children[1].style.fontSize = "350%";
+            document.getElementById("smileyList").children[2].style.fontSize = "150%";
+            break;
+        case 3:
+            document.getElementById("smileyList").children[0].style.fontSize = "150%";
+            document.getElementById("smileyList").children[1].style.fontSize = "150%";
+            document.getElementById("smileyList").children[2].style.fontSize = "350%";
+            break;
+    }
+
+    setTimeout(function () {
+        document.getElementById("smileySurvey").style.display = 'none';
+    }, 3000);
 }
