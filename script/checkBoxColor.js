0139944216693db7056570089d0e295b15c0d12d
Leo <leo.ebenezer@hotmail.com>
2017-11-03 16:10:07 +0100

    Add new classlists

--- script/checkBoxColor.js
+++ script/checkBoxColor.js
@@ -6,5 +6,23 @@ var handler = function(event) {
     }
 }
 
+/*
+function cbChange(obj) {
+    var cbs = document.getElementsByClassName("cb");
+    for (var i = 0; i < cbs.length; i++) {
+        cbs[i].checked = false;
+    }
+    obj.checked = true;
+}
+*/
+
+function myFunction1(x, _this) {
+  if (_this.checked) {
+    x.style.backgroundColor = 'green';
+  } else  {
+    x.style.backgroundColor = 'red';
+  }
+}
+
 var table = document.getElementById('table');
 table.addEventListener('click', handler);
