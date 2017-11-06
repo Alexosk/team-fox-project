f60a92bdd81618f8d44f34a514e383bd4ec9048c
Leo <leo.ebenezer@hotmail.com>
2017-11-06 15:06:22 +0100

    Add drop down and other fluff

--- script/app.js
+++ script/app.js
@@ -114,3 +114,35 @@ zone.event('iframeDone', function (xhr) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
   }
+
+
+
+
+/****DROPDOWN****/
+function myDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropBtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
