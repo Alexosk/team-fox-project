95154c106182e8ff4e2ce43e77e3e803f428d804
alexosk <alexsigurdar@gmail.com>
2017-11-16 14:11:03 +0100

    Add pages to admin and javascript on admin-classes

---
+++ script/student-list.js
@@ -0,0 +1,48 @@
+let students = [
+    'Armida Abrahamsson',
+    'Abbott Askman',
+    'Belle Björling',
+    'Balthazar Blank',
+    'Celine Carelius',
+    'Callum McRay',
+    'Disa Dahl',
+    'Damien Dunkels',
+    'Eddie Vedder',
+    'Fenja Fagher',
+    'Gabriel Gahn',
+    'Hampus Högberg',
+    'Igor Ingman',
+    'Jack the Ripper',
+    'Kean König',
+    'Laban Spöket',
+    'My Meling',
+    'Natanael Nostling',
+    'Odd Ottosson',
+    'Paul McCartney',
+    'Quentin Tarantino',
+    'Rafael Nadal',
+    'Selma Sandér',
+    'Tara Tarass',
+    'Valdemar Wolter',
+    'Xander Svensson',
+    'Zac Wilde',
+    'Zelda Johansson',
+    'Öivind Österberg',
+    'Örjan Östling',
+    'Örn Ärlig',
+    'Östen Åberg',
+    'Stu Dentson'
+
+];
+
+window.addEventListener('load', function(){
+let content = document.getElementById('content');
+let ul = document.createElement('ul');
+content.appendChild(ul);
+
+for (var i = 0; i < students.length; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = students[i];
+  ul.appendChild(li);
+}
+});
