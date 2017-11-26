e12fd816893772fd4baeef50f7f5fdb9378a5066
Alexosk <alexsigurdar@gmail.com>
2017-11-26 15:41:42 +0100

    Make a dropDown2.js for utvärderingar on teacher pages and delete old pages

---
+++ script/dropDown2.js
@@ -0,0 +1,17 @@
+function myDropdown2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropBtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
